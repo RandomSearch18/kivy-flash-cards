505f088c50d1761f996c614df856fbaac72d9b4d
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2024-07-02 10:23:09 +0100

    Add some buttons to the top

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 import kivy
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.app import App
 from kivy.uix.label import Label
 
 kivy.require("2.3.0")
+
+
+class FlashcardButton(Button):
+    def __init__(self, label: str, question: str, answer: str, **kwargs):
+        super(FlashcardButton, self).__init__(**kwargs)
+        self.text = label
+        self.question = question
+        self.answer = answer
+        self.bind(on_press=self.callback)
+
+    def callback(self, target: Button):
+        print('The "%s" flashcard was selected' % target.text)
 
 
 class FlashcardsScreen(BoxLayout):
@@ -15,10 +28,20 @@ class FlashcardsScreen(BoxLayout):
         self.top_buttons = BoxLayout(
             orientation="horizontal", size_hint_y=None, height=50
         )
-        self.top_buttons.add_widget(Label(text="Flashcard 1"))
-        self.top_buttons.add_widget(Label(text="Flashcard 2"))
-        self.top_buttons.add_widget(Label(text="Flashcard 3"))
-        self.top_buttons.add_widget(Label(text="Flashcard 4"))
+        self.top_buttons.add_widget(
+            FlashcardButton(
+                "RAM",
+                "What is the purpose of RAM?",
+                "Stores in-use data and instructions",
+            )
+        )
+        self.top_buttons.add_widget(
+            FlashcardButton(
+                "TCP/IP stack",
+                "What are the 4 layers in the TCP/IP stack?",
+                "Application, Transport, Internet, Link",
+            )
+        )
         self.add_widget(self.top_buttons)
 
         self.add_widget(Label(text="What is the purpose of RAM?"))
